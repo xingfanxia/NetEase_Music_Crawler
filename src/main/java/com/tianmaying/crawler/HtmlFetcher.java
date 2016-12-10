4437c8458ab6aa32672b66fe050c1237b695ac1f
xxf1995 <xiax@carleton.edu>
2016-12-09 23:06:02 -0600

    init and finishing the fetcher class

---
+++ src/main/java/com/tianmaying/crawler/HtmlFetcher.java
@@ -0,0 +1,23 @@
+package com.tianmaying.crawler;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+
+public class HtmlFetcher {
+
+    //What's going on here?
+    public String fetch(String url) {
+        try {
+            Connection.Response response = Jsoup.connect(url).timeout(6000).execute();
+            return response.statusCode() / 100 == 2 ? response.body() : null;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    
+    public static <T> void main(String[] args) throws Exception {
+        HtmlFetcher htmlFetcher = new HtmlFetcher();
+        System.out.println(htmlFetcher.fetch("http://music.163.com/#/discover/playlist/?order=hot&cat=%E5%85%A8%E9%83%A8&limit=35&offset=0"));
+    }
+
+}
